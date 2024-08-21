0b7e7db7553416939f26b70d4683fa2503e3717a
Breno Porto Pinheiro do Prado <brenoppprado@gmail.com>
2024-08-21 15:05:52 -0300

    atualizei os arquivos

---
+++ src/IphoneUsuario1.java
@@ -0,0 +1,10 @@
+public class IphoneUsuario1 {
+    public static void main(String[] args) {
+        Iphone Iphone = new Iphone();
+        Iphone.tocar();
+        Iphone.pausar();
+        Iphone.selecionarMusica("I wonder - Kayne West");
+        Iphone.ligar("4002-8922");
+        Iphone.exibirPagina("http://www.youtube.com");
+    }
+}
